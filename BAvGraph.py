8484b02f301db5ec3ade66d8b403fb0b0b15e9ea
Paul Leiva <54649136+paul-leiva@users.noreply.github.com>
2020-08-24 04:56:21 -0400

    Added code to account for traded players

--- BAvGraph.py
+++ BAvGraph.py
@@ -2,30 +2,29 @@ from bs4 import BeautifulSoup
 import requests
 import matplotlib
 
-print('Cody Bellinger')
-
-response = requests.get('https://www.baseball-reference.com/players/b/bellico01.shtml')
+print('Kenny Lofton')
+response = requests.get('https://www.baseball-reference.com/players/l/loftoke01.shtml')
 
 soup = BeautifulSoup(response.text, 'html.parser')
 
 table = soup.find("table", id='batting_standard')
-
 rows = table.find_all('tr', attrs = {'class':'full'})
+
+prevYear = 0
+data = []
 for row in rows:
-    cell = row.find('td', attrs={"data-stat":"batting_avg"})
-    print(cell.get_text())
-    #for cell in row.find_all('td'):
-        #print(cell.get_text())
+    Year = row.find('th', attrs={"data-stat":"year_ID"}).get_text()
+    if Year == prevYear:
+        pass
+    else:
+        prevYear = Year
+    Team = row.find('td', attrs={"data-stat":"team_ID"}).get_text()
+    League = row.find('td', attrs={"data-stat":"lg_ID"}).get_text()
+    BA = row.find('td', attrs={"data-stat":"batting_avg"}).get_text()
+    HR = row.find('td', attrs={"data-stat": "HR"}).get_text()
+    data.append([Year, Team, League, BA, HR])
+    print(Year + '|' + Team + '|' + League + '|' + HR + '|' + BA)
+    #for cell in row.find_all('td'): FROM EVERY ROW
+        #print(cell.get_text())      PRINT EVERY CELL
 
-
-tabledata = []
-
-#posts = soup.find_all(class_="full")
-
-#print(tabledata)
-"""
-for row in rows:
-    target_column = row.find_all('td', 'right ')
-    #target_column = row.find_all('attrs={"data-stat": "lg_ID"}')
-    print(target_column)
-"""
+print(data)
